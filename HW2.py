5e52f0356afda036e2870dc46ff370180e083493
AnastasiiaMaz <ashevchenko57@gmail.com>
2024-12-13 14:55:12 +0200

    No change

---
+++ HW2.py
@@ -0,0 +1,38 @@
+#5394
+number=int(input('Enter you number:')) # для 4 -го числа
+n1=number//1000
+n2=number%1000//100
+n3=number//10%10
+n4=number%10
+print(n1)
+print(n2)
+print(n3)
+print (n4)
+
+####
+
+#number=int(input('Enter you number:')) #если пользователь ввел 3-х значное число
+#n1=number//1000
+#n2=number%1000//100
+#n3=number//10%10
+#n4=number%10
+
+#print(n1)
+#print(n2)
+#print(n3)
+#print (n4)
+
+####
+
+#number=int(input('Enter you number:')) # для 5-го числа, для себя сделала,чтобы понимать алгоритм
+#n1=number // 10000
+#n2=number // 1000 % 10
+#n3=number //100 % 10
+#n4=number //10 % 10
+#n5=number % 10
+
+#print(n1)
+#print(n2)
+#print(n3)
+#print(n4)
+#print(n5)
